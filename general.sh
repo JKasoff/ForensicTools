d4fd0a8b7699b3747ab74eeee1d0c3043377243d
Jordan Kasoff <jordan7kasoff@gmail.com>
2023-02-25 18:35:42 -0500

    V2 Improved spacing and flags

--- general.sh
+++ general.sh
@@ -1,33 +1,36 @@
 # Forensic Script
 # Created By Jordan Kasoff (jmk9298@rit.edu)
-
-
-
-
-
-
-
 #Welcome Message
+
 echo "
 ===========================================
           Forensic Script V1.2
          Created By Jordan Kasoff
 ===========================================
 "
-echo "Please note, SUDO is required"
+
 
 #Global Settings
-while getopts ":bf:" OPTION; do
+while getopts "b:f:s:" OPTION; do
     case "${OPTION}" in
-          b) brief_output=1 #true (boolean)
-          echo "Brief Verbose Mode: Engaged";;
-        f) filename="$OPTARG"
-          echo "$filename";;
-
-
-        \?) # Invalid option
-         echo "Error: Invalid option"
-         exit;;
+        b)
+            brief_output=1 #true (boolean)
+            echo "Brief Verbose Mode: Engaged"
+            ;;
+        f)
+            filename="$OPTARG"
+            filename=${filename// /_}
+            filename=${filename//[^a-zA-Z0-9_.]/}
+            ;;
+        s)
+            sudo_commands=1
+            # echo "Please note, SUDO is required"
+            ;;
+        \?)
+            # Invalid option
+            echo "Error: Invalid option"
+            exit
+            ;;
     esac
 done
 
@@ -36,29 +39,49 @@ done
 #Thought: Ideally Script will be on USB, and this will save report on same media
 #Get directory of the script, create report file in same location
 redirect_output() {
-	location=$(pwd)
-	if [-z ${filename}];
-	then 	filename="report.txt";
+  echo ""
+	#location=$(pwd) Former Local Definition
+
+  #File Creation Logic
+  # Check if flag was not used, then pass basic name in same folder
+	if [ -z "${filename}" ];
+	then filename="report.txt";
   fi
-	final_location=$location"/"$filename
+
+	#final_location=$location"/"$filename Former Local Definition
+	final_location=$filename
+
 	if test -f "$final_location"; then #check if file exists
 		echo "$final_location exists, rewriting"
+		echo "File Writing @ " $(pwd)"/"$final_location
 	else
 		touch "$final_location" #create file if it does not
-		echo "File Created"
+		echo "File Created @ " $(pwd)"/"$final_location
 	fi
+
+  #Identity Reporter
   echo "Forensic Report"  > "$final_location" #rewrite file with title
 	identity=$(whoami)
 	echo "Run As: "  "$identity">> "$final_location" #rewrite file with title
-	echo  " " >> "$final_location" #append space to file
-}
-redirect_output
 
+  #Brief Mode Report Checker
+	if [ "$brief_output" -eq "1" ];
+	then
+	  echo  "Brief Verbose Mode: Engaged" >> "$final_location"
+	fi
+
+  printf  "\n\n" >> "$final_location" #append space to file
+  echo "Setup Complete"
+
+}
+
+redirect_output
 
 # Send time data to final location
 get_time_data() {
 	curr_time=$(date) #save current time (date) to variable "curr_time"
 	time_up=$(uptime)
+	printf "Time Information\n================\n" >> "$final_location"
 	echo "System Time: " "$curr_time" >> "$final_location" # append variable to file
 	echo "Uptime:" "$time_up" >> "$final_location"
 	echo ""
@@ -73,6 +96,8 @@ get_OS_data(){
 	cpu_info=$(lscpu |grep "Model name")
 	typical_name=$(hostnamectl |grep "Icon name")
 	kernel_name=$(hostnamectl |grep "Kernel")
+
+	printf "\nOS Information\n================\n" >> "$final_location"
 	echo $operating_name >> $final_location
 	echo $cpu_info >> $final_location
 	echo $typical_name >> $final_location
@@ -88,13 +113,12 @@ get_system_data(){
 	architecture=$(hostnamectl |grep "Architecture")
 	memory=$(free -h)
 	storage=$(df -h)
-	partition=$(sudo fdisk -l) #requires sudo permission
 	host_name=$(hostname)
 	domain_name=$(domainname)
 
 	echo $architecture >> $final_location
 	echo " " >> $final_location
-	echo "Memory Information" >> $final_location
+	printf "Architecture Information\n================" >> $final_location
 	echo " " >> $final_location
 	printf "%s\n" "$memory" >> $final_location #preserve line spacing
 	echo " " >> $final_location
@@ -102,26 +126,103 @@ get_system_data(){
 	echo " " >> $final_location
 	printf "%s\n" "$storage" >> $final_location #preserve line spacing
 	echo " " >> $final_location
-	echo "Partition Information" >> $final_location
-	echo " " >> $final_location
-	printf "%s\n" "$partition" >> $final_location
-	echo " " >> $final_location
+
+	if [ "$sudo_commands" = 1 ];
+	then
+    partition=$(sudo fdisk -l) #requires sudo permission
+    echo "Partition Information" >> $final_location
+	  echo " " >> $final_location
+	  printf "%s\n" "$partition" >> $final_location
+	fi
+
 	echo "Hostname: " $host_name >> $final_location
 	if [[ "$domain_name" = "(none)" ]]; then
 	 	echo "No Domain Name" >> $final_location
 	else
 		echo $domain_name >> $final_location
 	fi
+
 	echo ""
 	echo "Wrote System Data"
+
 }
 
 get_system_data
 
 
+get_user_data(){
+	logged_in=$(who)
+	logged_out=$(last)
+	root_uid=$(awk -F: '($3 == 0) {printf "%s:%s\n",$1,$3}' /etc/passwd)
+
+  printf "\nUser Information\n================" >> $final_location
+	echo " " >> $final_location
+	echo " " >> $final_location
+	echo "Users Logged in" >> $final_location
+	printf "%s\n" "$logged_in" >> $final_location
+	echo " " >> $final_location
+	echo "User Session History: " >> $final_location
+	printf "%s\n" "$logged_out" >> $final_location
+	echo " " >> $final_location
+	echo "Users with UID=0: " >> $final_location
+	printf "%s\n" "$root_uid" >> $final_location
+	echo ""
+	echo "" >> $final_location
+	echo "SUID - Root Files: " >> $final_location
+	if [ "$brief_output" -eq "0" ];
+	then
+		suid_files=$(find / -uid 0 -perm -4000 2>/dev/null)
+	else
+		suid_files=$(find / -uid 0 -perm -4000 2>/dev/null|head -n 15)
+	fi
+	printf "%s\n"  "$suid_files" >> $final_location
+	echo "Wrote User Data"
+}
+
+get_user_data
+
+
+get_process_data(){
+	echo " " >> $final_location
+	echo " " >> $final_location
+	echo "Process List:" >> $final_location
+	if [ "$brief_output" -eq "0" ];
+	then
+		proc_list=$(ps ax)
+	else
+		proc_list=$(ps ax|head -n 5)
+	fi
+	printf "%s\n"  "$proc_list" >> $final_location
+	echo ""
+
+	echo " " >> $final_location
+	echo "NetCat Files List:" >> $final_location
+	if [ "$brief_output" -eq "0" ];
+	then
+		net_list=$(lsof -c nc)
+	else
+		net_list=$(lsof -c nc |head -n 5)
+	fi
+	printf "%s\n"  "$net_list" >> $final_location
+
+	echo " " >> $final_location
+	echo "Unlinked Files List:" >> $final_location
+	if [ "$brief_output" -eq "0" ];
+	then
+		delete_list=$(lsof +L1)
+	else
+		delete_list=$(lsof +L1 |head -n 5)
+	fi
+	printf "%s\n"  "$delete_list" >> $final_location
+
+	echo "Wrote Process Data"
+}
+
+get_process_data
+
 get_network_data(){
 	echo "" >> $final_location
-	echo "Network Information:" >> $final_location
+	printf "Network Information\n==============:" >> $final_location
 	for interface in $(ls /sys/class/net)
 	do
 
@@ -154,79 +255,6 @@ get_network_data(){
 get_network_data
 
 
-get_user_data(){
-	logged_in=$(who)
-	logged_out=$(last)
-	root_uid=$(awk -F: '($3 == 0) {printf "%s:%s\n",$1,$3}' /etc/passwd)
-
-	echo " " >> $final_location
-	echo " " >> $final_location
-	echo "Users Logged in" >> $final_location
-	printf "%s\n" "$logged_in" >> $final_location
-	echo " " >> $final_location
-	echo "User Session History: " >> $final_location
-	printf "%s\n" "$logged_out" >> $final_location
-	echo " " >> $final_location
-	echo "Users with UID=0: " >> $final_location
-	printf "%s\n" "$root_uid" >> $final_location
-	echo ""
-	echo "" >> $final_location
-	echo "SUID - Root Files: " >> $final_location
-	if [ "$brief_output" -eq "0" ];
-	then
-		suid_files=$(find / -uid 0 -perm -4000 2>/dev/null)
-	else
-		suid_files=$(find / -uid 0 -perm -4000 2>/dev/null|head -n 15)
-	fi
-	printf "%s\n"  "$suid_files" >> $final_location
-	echo "Wrote User Data"
-
-}
-
-get_user_data
-
-
-get_process_data(){
-	echo " " >> $final_location
-	echo " " >> $final_location
-	echo "Process List:" >> $final_location
-	if [ "$brief_output" -eq "0" ];
-	then
-		proc_list=$(ps ax)
-	else
-		proc_list=$(ps ax|head -n 5)
-	fi
-	printf "%s\n"  "$proc_list" >> $final_location
-	echo ""
-
-	echo " " >> $final_location
-	echo "NetCat Files List:" >> $final_location
-	if [ "$brief_output" -eq "0" ];
-	then
-		net_list=$(lsof -c nc)
-	else
-		net_list=$(lsof -c nc |head -n 5)
-	fi
-	printf "%s\n"  "$net_list" >> $final_location
-	echo ""
-
-	echo " " >> $final_location
-	echo "Unlinked Files List:" >> $final_location
-	if [ "$brief_output" -eq "0" ];
-	then
-		delete_list=$(lsof +L1)
-	else
-		delete_list=$(lsof +L1 |head -n 5)
-	fi
-	printf "%s\n"  "$delete_list" >> $final_location
-	echo ""
-
-	echo "Wrote Process Data"
-}
-
-get_process_data
-
-
 get_other_data(){
 	echo " " >> $final_location
 	echo " " >> $final_location
@@ -240,10 +268,13 @@ get_other_data(){
 	printf "%s\n"  "$modify_list" >> $final_location
 	echo "" >> $final_location
 
+	if [ "$sudo_commands" = 1 ];
+	then
 	echo "Root Cron Tasks Sched:" >> $final_location
 	task_list=$(sudo crontab -u root -l)
 	printf "%s\n"  "$task_list" >> $final_location
 	echo "" >> $final_location
+	fi
 
 	echo "SSH connections" >> $final_location
 	ssh_list=$(netstat -a | grep ssh)
@@ -262,11 +293,13 @@ get_other_data(){
 
 	echo ""
 
-	echo "Wrote Others Data"
+	echo "Wrote Extra Data"
 
 
 }
 
 get_other_data
 
+echo " "
 echo "Report Complete"
+exit
